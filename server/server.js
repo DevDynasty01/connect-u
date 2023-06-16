e4c81e9c3bb780e6a66155daccf4a67655e86307
Farjana Akter <fakter@xpanxion.com>
2023-06-16 19:03:50 -0400

    changes from farjana

--- server/server.js
+++ server/server.js
@@ -2,8 +2,6 @@ const express = require('express');
 const pool = require('./pool').pool;
 const cors = require('cors');
 
-
-
 const PORT=8080;
 const app = express(); 
 const corsOptions = {origin:['http://localhost:3000'], optionsSuccessStatus : 200}
@@ -11,16 +9,18 @@ const corsOptions = {origin:['http://localhost:3000'], optionsSuccessStatus : 20
 app.use(cors());
 app.use(express.json());
 
-app.get('/employees', cors(corsOptions), async (req, res) => {
-    const [name] = await pool.query('SELECT * FROM usersdb')
+// app.get('/employees', cors(corsOptions), async (req, res) => {
+//     const [name] = await pool.query('SELECT * FROM usersdb')
+//     res.send(name);
+// })
+
+
+app.get('/employerTask', cors(corsOptions), async (req, res) => {
+    const [name] = await pool.query('SELECT * FROM tasks')
     res.send(name);
 })
 
-// app.post('/employees', cors(corsOptions), async (req, res) => {
-//     let { name, task } = req.body; 
-//     const [newTask] = await pool.execute('INSERT INTO users (task) VALUES (?) WHERE name = ?', [task, name])
-//     res.send(newTask);
-// })
+
 
 app.listen(PORT, () => {
     console.log(`Express web API running on port ${PORT}.`)
