feecab9da46a240851b224cdf0d0a31098d325b2
Valerie PerezPimentel <vperezpimentel@xpanxion.com>
2023-06-18 00:35:55 -0400

    start time tracking

--- src/Components/TimeTracking.js
+++ src/Components/TimeTracking.js
@@ -1,26 +1,50 @@
 import React, { useState } from 'react'
 
 export const TimeTracking = () => {
-  let [start, setStart] = useState([]);
-  let [lunch, setLunch] = useState([]);
-  let [end, setEnd] = useState([]);
+  let [start, setStart] = useState();
+  let [lunch, setLunch] = useState();
+  let [backLunch, setBackLunch] = useState();
+  let [end, setEnd] = useState();
 
-  const recordTime = (timeOfDay) =>{
-    if (timeOfDay === 'start') setStart(new Date().toUTCString())
-    if (timeOfDay === 'lunch') setLunch(new Date().toUTCString())
-    else setEnd(new Date().toUTCString())
+  const startTime = () =>{
+    let time = new Date().toUTCString()
+    setStart(time)
+
+  }
+
+  const lunchTime = () =>{
+    let time = new Date().toUTCString()
+    setLunch(time)
+    
+  }
+  const backFromLunch = () =>{
+    let time = new Date().toUTCString()
+    setBackLunch(time)
+
+  }
+
+  const endTime = () =>{
+    let time = new Date().toUTCString()
+    setEnd(time)
+    
   }
 
   
+
+  
+  
   return (
     <div>
-      <button onClick={recordTime =>('start')}>Start of Day</button>
+      <button onClick={startTime}>Start of Day 🌞</button>
       Start of day: {start}
       <br/>
-      <button onClick={recordTime =>('lunch')}>Lunch</button>
+      <button onClick={lunchTime}>Lunch 🥙</button>
       Lunch: {lunch}
       <br/>
-      <button onClick={recordTime =>('end')}>End of Day</button>
+      <button onClick={backFromLunch}>Back from to work 👨‍💻</button>
+      Back From lunch: {backLunch}
+      <br/>
+      <button onClick={endTime}>End of Day 💃</button>
       End of day: {end}
       
     </div>
